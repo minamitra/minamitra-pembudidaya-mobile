6062d368030067f522f34aa4b3b94e7c8d7af803
adrisabik <adrisabik@gmail.com>
2024-08-01 22:14:54 +0700

    🔧 add injection

---
+++ lib/core/utils/injection.dart
@@ -0,0 +1,9 @@
+class Injection {
+  static const fontFamily = "Inter";
+  // static final HttpClient httpClient = AppHttpClient.create();
+  // static final HeaderProvider headerProvider = AppHeaderProvider.create();
+  // static const host = "";
+  // static const quranHost = "";
+  // static const prayerHost = "";
+  // static const kGoogleApiKey = "";
+}
