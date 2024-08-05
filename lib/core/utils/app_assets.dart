8e9a5fed16cab4f6f86218b24034b185b340b04d
adrisabik <adrisabik@gmail.com>
2024-08-05 22:05:08 +0700

    🍱 add assets

--- lib/core/utils/app_assets.dart
+++ lib/core/utils/app_assets.dart
@@ -9,4 +9,28 @@ class AppAssets {
   // Image
   static const String circleImage = "$image/circle-image.png";
   static const String fisherManImage = "$image/fisherman-image.png";
+
+  // Dashboard
+  static const String homeIcon = "assets/icons/home-icon.png";
+  static const String tansactionIcon = "assets/icons/transaction-icon.png";
+  static const String hearthIcon = "assets/icons/hearth-icon.png";
+  static const String userIcon = "assets/icons/user-icon.png";
+
+  // Home
+  static const String banner1Image = "assets/images/banner-1-image.png";
+  static const String banner2Image = "assets/images/banner-2-image.png";
+  static const String banner3Image = "assets/images/banner-3-image.png";
+  static const String basketIcon = "assets/icons/basket-icon.png";
+  static const String bellIcon = "assets/icons/bell-icon.png";
+  static const String walletIcon = "assets/icons/wallet-icon.png";
+  static const String scanIcon = "assets/icons/scan-icon.png";
+  static const String historyIcon = "assets/icons/history-icon.png";
+  static const String starIcon = "assets/icons/star-icon.png";
+  static const String medalSilverIcon = "assets/icons/medal-silver-icon.png";
+  static const String medalGoldIcon = "assets/icons/medal-gold-icon.png";
+  static const String crownIcon = "assets/icons/crown-icon.png";
+  static const String speakerIcon = "assets/icons/speaker-icon.png";
+  static const String locationIcon = "assets/icons/location-icon.png";
+  static const String ticketIcon = "assets/icons/ticket-icon.png";
+  static const String bagIcon = "assets/icons/bag-icon.png";
 }
