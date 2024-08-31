de0ab4bc5ed07a6d73307dcfe9f5867caa8254b4
adrisabik <adrisabik@gmail.com>
2024-08-31 14:05:29 +0700

    🎨 add routing to activity cycle page in detail activity view

--- lib/feature/detail_activity/view/detail_activity_view.dart
+++ lib/feature/detail_activity/view/detail_activity_view.dart
@@ -4,6 +4,7 @@ import 'package:minamitra_pembudidaya_mobile/core/themes/app_color.dart';
 import 'package:minamitra_pembudidaya_mobile/core/utils/app_assets.dart';
 import 'package:minamitra_pembudidaya_mobile/core/utils/app_transition.dart';
 import 'package:minamitra_pembudidaya_mobile/feature/activity_activities/views/activity_activities_page.dart';
+import 'package:minamitra_pembudidaya_mobile/feature/activity_cycle/views/activity_cycle_page.dart';
 import 'package:minamitra_pembudidaya_mobile/feature/activity_incident/views/activity_incident_page.dart';
 import 'package:minamitra_pembudidaya_mobile/feature/monitoring/view/monitoring_page.dart';
 import 'package:minamitra_pembudidaya_mobile/main.dart';
@@ -111,9 +112,14 @@ class _DetailActivityViewState extends State<DetailActivityView> {
                 },
               ),
               bottomActionItem(
-                'Panen',
+                'Siklus',
                 AppAssets.cartIcon,
-                onTap: () {},
+                onTap: () {
+                  Navigator.of(context).push(AppTransition.pushTransition(
+                    const ActivityCyclePage(),
+                    ActivityCyclePage.routeSettings(),
+                  ));
+                },
               ),
             ],
           ),
