17db8b90df238901fb4a5da7a4dead3dd77eb880
adrisabik <adrisabik@gmail.com>
2024-09-29 19:02:42 +0700

    🎨 update activity sampling detail page

--- lib/feature/activity_sampling_detail/views/activity_sampling_detail_page.dart
+++ lib/feature/activity_sampling_detail/views/activity_sampling_detail_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:minamitra_pembudidaya_mobile/core/components/app_bar.dart';
+import 'package:minamitra_pembudidaya_mobile/core/utils/app_transition.dart';
 import 'package:minamitra_pembudidaya_mobile/feature/activity_activities/repositories/sampling_response.dart';
+import 'package:minamitra_pembudidaya_mobile/feature/activity_sampling_add/view/activity_sampling_add_page.dart';
 import 'package:minamitra_pembudidaya_mobile/feature/activity_sampling_detail/views/activity_sampling_detail_view.dart';
 import 'package:minamitra_pembudidaya_mobile/main.dart';
 
@@ -17,10 +19,20 @@ class ActivitySamplingDetailPage extends StatelessWidget {
     return Scaffold(
       appBar: appDefaultAppBar(
         context,
-        "Detail Perlakuan",
+        "Detail Sampling",
         actions: [
           InkWell(
-            onTap: () {},
+            onTap: () {
+              Navigator.of(context).push(AppTransition.pushTransition(
+                ActivitySamplingAddPage(
+                  int.parse(data.fishpondId ?? "1"),
+                  int.parse(data.fishpondcycleId ?? "1"),
+                  isEdit: true,
+                  data: data,
+                ),
+                ActivitySamplingAddPage.routeSettings,
+              ));
+            },
             child: Text(
               "Edit",
               style: appTextTheme(context)
