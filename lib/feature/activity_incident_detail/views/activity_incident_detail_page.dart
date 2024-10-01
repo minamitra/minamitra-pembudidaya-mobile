6a856c9fb2dcd5770f1b03d23ffbcca96fc079d4
adrisabik <adrisabik@gmail.com>
2024-10-02 01:51:09 +0700

    🎨 update activity incident detail page

--- lib/feature/activity_incident_detail/views/activity_incident_detail_page.dart
+++ lib/feature/activity_incident_detail/views/activity_incident_detail_page.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:minamitra_pembudidaya_mobile/core/components/app_bar.dart';
-import 'package:minamitra_pembudidaya_mobile/feature/activity_incident/repositories/incident_data.dart';
+import 'package:minamitra_pembudidaya_mobile/core/utils/app_transition.dart';
+import 'package:minamitra_pembudidaya_mobile/feature/activity_incident/repositories/incident_response.dart';
+import 'package:minamitra_pembudidaya_mobile/feature/activity_incident_add/views/activity_incident_add_page.dart';
 import 'package:minamitra_pembudidaya_mobile/feature/activity_incident_detail/views/activity_incident_detail_view.dart';
+import 'package:minamitra_pembudidaya_mobile/main.dart';
 
 class ActivityIncidentDetailPage extends StatelessWidget {
-  final Incident incident;
+  final IncidentResponseData incident;
   const ActivityIncidentDetailPage(this.incident, {super.key});
 
   static RouteSettings routeSettings() =>
@@ -16,6 +19,28 @@ class ActivityIncidentDetailPage extends StatelessWidget {
       appBar: appDefaultAppBar(
         context,
         "Detail Kejadian",
+        actions: [
+          InkWell(
+            onTap: () {
+              Navigator.of(context).push(AppTransition.pushTransition(
+                ActivityIncidentAddPage(
+                  int.parse(incident.fishpondId!),
+                  int.parse(incident.fishpondcycleId!),
+                  isEdit: true,
+                  data: incident,
+                ),
+                ActivityIncidentAddPage.routeSettings,
+              ));
+            },
+            child: Text(
+              "Edit",
+              style: appTextTheme(context)
+                  .bodyMedium
+                  ?.copyWith(color: Colors.white),
+            ),
+          ),
+          const SizedBox(width: 16.0),
+        ],
       ),
       body: ActivityIncidentDetailView(incident),
     );
