21265c99eb261d37bf702ab8ffb754646eb4d1a5
adrisabik <adrisabik@gmail.com>
2024-10-07 16:00:00 +0700

    👔 udpate add pond logics

--- lib/feature/add_pond/logic/add_pond_state.dart
+++ lib/feature/add_pond/logic/add_pond_state.dart
@@ -5,21 +5,25 @@ class AddPondState extends Equatable {
     this.status = GlobalState.initial,
     this.errorMessage = '',
     this.index = 0,
+    this.updatePondPayload,
   });
 
   final GlobalState status;
   final String errorMessage;
   final int index;
+  final UpdatePondPayload? updatePondPayload;
 
   AddPondState copyWith({
     GlobalState? status,
     String? errorMessage,
     int? index,
+    UpdatePondPayload? updatePondPayload,
   }) {
     return AddPondState(
       status: status ?? this.status,
       errorMessage: errorMessage ?? this.errorMessage,
       index: index ?? this.index,
+      updatePondPayload: updatePondPayload ?? this.updatePondPayload,
     );
   }
 
@@ -28,5 +32,6 @@ class AddPondState extends Equatable {
         status,
         errorMessage,
         index,
+        updatePondPayload,
       ];
 }
