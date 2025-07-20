64705b2fcb1e7f084d0b47247aa74c53741134d0
adrisabik <adrisabik@gmail.com>
2025-07-20 20:49:52 +0700

    🔧 add mat alternatives

---
+++ lib/feature/ahp/repositories/mat_alternatives.dart
@@ -0,0 +1,326 @@
+import 'package:matrices/matrices.dart';
+
+Matrix matAlternativeStarter1C01 = Matrix.fromList([
+  [1, 0.5, 0.5, 0.333],
+  [2, 1, 1, 0.5],
+  [0.5, 1, 1, 2],
+  [3, 2, 2, 1],
+]);
+
+Matrix matAlternativeStarter1C02 = Matrix.fromList([
+  [1, 1, 1, 2],
+  [1, 1, 1, 2],
+  [1, 1, 1, 2],
+  [0.5, 0.5, 0.5, 1],
+]);
+
+Matrix matAlternativeStarter1C03 = Matrix.fromList([
+  [1, 1, 1, 1],
+  [1, 1, 1, 1],
+  [1, 1, 1, 1],
+  [1, 1, 1, 1],
+]);
+
+Matrix matAlternativeStarter1C04 = Matrix.fromList([
+  [1, 0.333, 0.333, 0.333],
+  [3, 1, 1, 1],
+  [3, 1, 1, 1],
+  [3, 1, 1, 1],
+]);
+
+Matrix matAlternativeStarter1C05 = Matrix.fromList([
+  [1, 0.5, 0.5, 0.5],
+  [2, 1, 1, 1],
+  [2, 1, 1, 1],
+  [2, 1, 1, 1],
+]);
+
+Matrix matAlternativeStarter1C06 = Matrix.fromList([
+  [1, 1, 1, 2],
+  [1, 1, 1, 2],
+  [1, 1, 1, 2],
+  [0.5, 0.5, 0.5, 1],
+]);
+
+Matrix matAlternativeStarter2C01 = Matrix.fromList([
+  [1, 0.333, 0.5, 0.333, 0.5],
+  [3, 1, 2, 1, 1],
+  [2, 0.5, 1, 0.5, 0.5],
+  [3, 1, 2, 1, 1],
+  [3, 1, 2, 1, 1],
+]);
+
+Matrix matAlternativeStarter2C02 = Matrix.fromList([
+  [1, 3, 2, 3, 3],
+  [0.333, 1, 0.5, 1, 1],
+  [0.5, 2, 1, 2, 2],
+  [0.333, 1, 0.5, 1, 1],
+  [0.333, 1, 0.5, 1, 1],
+]);
+
+Matrix matAlternativeStarter2C03 = Matrix.fromList([
+  [1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1],
+]);
+
+Matrix matAlternativeStarter2C04 = Matrix.fromList([
+  [1, 0.5, 1, 0.5, 0.5],
+  [2, 1, 2, 1, 1],
+  [1, 0.5, 1, 0.5, 0.5],
+  [2, 1, 2, 1, 1],
+  [2, 1, 2, 1, 1],
+]);
+
+Matrix matAlternativeStarter2C05 = Matrix.fromList([
+  [1, 1, 1, 0.5, 0.5],
+  [1, 1, 1, 0.5, 0.5],
+  [1, 1, 1, 0.5, 0.5],
+  [2, 2, 2, 1, 1],
+  [2, 2, 2, 1, 1],
+]);
+
+Matrix matAlternativeStarter2C06 = Matrix.fromList([
+  [1, 0.5, 0.5, 0.5, 0.5],
+  [2, 1, 1, 1, 1],
+  [2, 1, 1, 1, 1],
+  [2, 1, 1, 1, 1],
+  [2, 1, 1, 1, 1],
+]);
+
+Matrix matAlternativeStarter3C01 = Matrix.fromList([
+  [1, 0.5, 0.5, 2, 2, 2],
+  [2, 1, 1, 3, 3, 3],
+  [2, 1, 1, 3, 3, 3],
+  [0.5, 0.333, 0.333, 1, 1, 1],
+  [0.5, 0.333, 0.333, 1, 1, 1],
+  [0.5, 0.333, 0.333, 1, 1, 1],
+]);
+
+Matrix matAlternativeStarter3C02 = Matrix.fromList([
+  [1, 2, 2, 0.5, 0.5, 0.5],
+  [0.5, 1, 1, 0.333, 0.333, 0.333],
+  [0.5, 1, 1, 0.333, 0.333, 0.333],
+  [2, 3, 3, 1, 1, 1],
+  [2, 3, 3, 1, 1, 1],
+  [2, 3, 3, 1, 1, 1],
+]);
+
+Matrix matAlternativeStarter3C03 = Matrix.fromList([
+  [1, 1, 1, 3, 3, 3],
+  [1, 1, 1, 3, 3, 3],
+  [1, 1, 1, 3, 3, 3],
+  [0.333, 0.333, 0.333, 1, 1, 1],
+  [0.333, 0.333, 0.333, 1, 1, 1],
+  [0.333, 0.333, 0.333, 1, 1, 1],
+]);
+
+Matrix matAlternativeStarter3C04 = Matrix.fromList([
+  [1, 1, 1, 0.333, 0.333, 0.333],
+  [1, 1, 1, 0.333, 0.333, 0.333],
+  [1, 1, 1, 0.333, 0.333, 0.333],
+  [3, 3, 3, 1, 1, 1],
+  [3, 3, 3, 1, 1, 1],
+  [3, 3, 3, 1, 1, 1],
+]);
+
+Matrix matAlternativeStarter3C05 = Matrix.fromList([
+  [1, 1, 1, 0.5, 1, 1],
+  [1, 1, 1, 0.5, 1, 1],
+  [1, 1, 1, 0.5, 1, 1],
+  [2, 2, 2, 1, 2, 2],
+  [1, 1, 1, 0.5, 1, 1],
+  [1, 1, 1, 0.5, 1, 1],
+]);
+
+Matrix matAlternativeStarter3C06 = Matrix.fromList([
+  [1, 1, 1, 0.5, 0.5, 0.5],
+  [1, 1, 1, 0.5, 0.5, 0.5],
+  [1, 1, 1, 0.5, 0.5, 0.5],
+  [2, 2, 2, 1, 1, 1],
+  [2, 2, 2, 1, 1, 1],
+  [2, 2, 2, 1, 1, 1],
+]);
+
+Matrix matAlternativeGrowerC01 = Matrix.fromList([
+  [1, 0.5, 0.5, 0.5, 2],
+  [2, 0.5, 0.5, 1, 3],
+  [2, 2, 1, 1, 3],
+  [2, 2, 1, 1, 3],
+  [0.5, 0.333, 0.333, 0.333, 1],
+]);
+
+Matrix matAlternativeGrowerC02 = Matrix.fromList([
+  [1, 1, 2, 2, 0.5],
+  [1, 1, 2, 2, 0.5],
+  [0.5, 0.5, 1, 1, 0.333],
+  [0.5, 0.5, 1, 1, 0.333],
+  [2, 2, 3, 3, 1],
+]);
+
+Matrix matAlternativeGrowerC03 = Matrix.fromList([
+  [1, 1, 1, 1, 0.333],
+  [1, 1, 1, 1, 0.333],
+  [1, 1, 1, 1, 0.333],
+  [1, 1, 1, 1, 0.333],
+  [3, 3, 3, 3, 1],
+]);
+
+Matrix matAlternativeGrowerC04 = Matrix.fromList([
+  [1, 1, 1, 1, 0.333],
+  [1, 1, 1, 1, 0.333],
+  [1, 1, 1, 1, 0.333],
+  [1, 1, 1, 1, 0.333],
+  [3, 3, 3, 3, 1],
+]);
+
+Matrix matAlternativeGrowerC05 = Matrix.fromList([
+  [1, 1, 1, 1, 0.5],
+  [1, 1, 1, 1, 0.5],
+  [1, 1, 1, 1, 0.5],
+  [1, 1, 1, 1, 0.5],
+  [2, 2, 2, 2, 1],
+]);
+
+Matrix matAlternativeGrowerC06 = Matrix.fromList([
+  [1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1],
+]);
+
+Matrix matAlternativeFinisherC01 = Matrix.fromList([
+  [1, 0.5, 0.5, 0.5, 2],
+  [2, 0.5, 0.5, 1, 3],
+  [2, 2, 1, 1, 3],
+  [2, 2, 1, 1, 3],
+  [0.5, 0.333, 0.333, 0.333, 1],
+]);
+
+Matrix matAlternativeFinisherC02 = Matrix.fromList([
+  [1, 1, 2, 2, 0.5],
+  [1, 1, 2, 2, 0.5],
+  [0.5, 0.5, 1, 1, 0.333],
+  [0.5, 0.5, 1, 1, 0.333],
+  [2, 2, 3, 3, 1],
+]);
+
+Matrix matAlternativeFinisherC03 = Matrix.fromList([
+  [1, 1, 1, 1, 0.333],
+  [1, 1, 1, 1, 0.333],
+  [1, 1, 1, 1, 0.333],
+  [1, 1, 1, 1, 0.333],
+  [3, 3, 3, 3, 1],
+]);
+
+Matrix matAlternativeFinisherC04 = Matrix.fromList([
+  [1, 1, 1, 1, 0.333],
+  [1, 1, 1, 1, 0.333],
+  [1, 1, 1, 1, 0.333],
+  [1, 1, 1, 1, 0.333],
+  [3, 3, 3, 3, 1],
+]);
+
+Matrix matAlternativeFinisherC05 = Matrix.fromList([
+  [1, 1, 1, 1, 0.5],
+  [1, 1, 1, 1, 0.5],
+  [1, 1, 1, 1, 0.5],
+  [1, 1, 1, 1, 0.5],
+  [2, 2, 2, 2, 1],
+]);
+
+Matrix matAlternativeFinisherC06 = Matrix.fromList([
+  [1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1],
+]);
+
+List<Matrix> listAlternativeStarter1 = [
+  matAlternativeStarter1C01,
+  matAlternativeStarter1C02,
+  matAlternativeStarter1C03,
+  matAlternativeStarter1C04,
+  matAlternativeStarter1C05,
+  matAlternativeStarter1C06,
+];
+
+List<Matrix> listAlternativeStarter2 = [
+  matAlternativeStarter2C01,
+  matAlternativeStarter2C02,
+  matAlternativeStarter2C03,
+  matAlternativeStarter2C04,
+  matAlternativeStarter2C05,
+  matAlternativeStarter2C06,
+];
+
+List<Matrix> listAlternativeStarter3 = [
+  matAlternativeStarter3C01,
+  matAlternativeStarter3C02,
+  matAlternativeStarter3C03,
+  matAlternativeStarter3C04,
+  matAlternativeStarter3C05,
+  matAlternativeStarter3C06,
+];
+
+List<Matrix> listAlternativeGrower = [
+  matAlternativeGrowerC01,
+  matAlternativeGrowerC02,
+  matAlternativeGrowerC03,
+  matAlternativeGrowerC04,
+  matAlternativeGrowerC05,
+  matAlternativeGrowerC06,
+];
+
+List<Matrix> listAlternativeFinisher = [
+  matAlternativeFinisherC01,
+  matAlternativeFinisherC02,
+  matAlternativeFinisherC03,
+  matAlternativeFinisherC04,
+  matAlternativeFinisherC05,
+  matAlternativeFinisherC06,
+];
+
+List<String> alternativeNameStarter1 = [
+  'Prima Feed PF500',
+  'Prima Feed PF800',
+  'Prima Feed PF1000',
+  'Haida HDMA1 0,8',
+];
+
+List<String> alternativeNameStarter2 = [
+  'Haida HDMA1 0.8',
+  'Haida HDPIN-B -2',
+  'Haida HDPIL-B -2',
+  'Haida HDPIP -2',
+  'Haida HDPIP -3',
+];
+
+List<String> alternativeNameStarter3 = [
+  'Super Patin',
+  'Pakan Siap Cetak (PSC) (2:1)',
+  'Pakan Siap Cetak (PSC) (1:1)',
+  'Haida HDPIP-3',
+  'Haida HDPIN-B -3',
+  'Haida HDPIN-B -2',
+];
+
+List<String> alternativeNameGrower = [
+  'Super Patin',
+  'PSC (Pakan Siap Cetak)',
+  'Pakan Siap Cetak (PSC) (2:1)',
+  'Pakan Siap Cetak (PSC) (1:1)',
+  'Haida HDPIP-3',
+];
+
+List<String> alternativeNameFinisher = [
+  'Super Patin',
+  'PSC (Pakan Siap Cetak)',
+  'Pakan Siap Cetak (PSC) (2:1)',
+  'Pakan Siap Cetak (PSC) (1:1)',
+  'Haida HDPIP-3',
+];
